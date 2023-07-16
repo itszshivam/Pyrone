13a50c06277ab3de0a1dd9b882775e30ab3d7fe7
itszshivam <139079474+itszshivam@users.noreply.github.com>
2023-07-16 12:50:24 +0530

    Update pyrone.py

--- pyrone.py
+++ pyrone.py
@@ -90,7 +90,7 @@ async def pyrone(client: Client, message: Message):
         for word in ONE_WORDS:
             await client.send_chat_action(chat_id, "typing")
             await client.send_message(chat_id, word, reply_to_message_id=ruser)
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0.01)
     except FloodWait:
         pass
 
